f0fc6357094baef967ee4e723dfb2435aa60fb29
Nikita Merzlyakov <merzlyakov02@gmail.com>
2023-06-02 05:05:34 +0300

    added deploy files

---
+++ application/setup.sh
@@ -0,0 +1,6 @@
+docker-compose down -v --remove-orphans --rmi all
+docker-compose up -d --build
+docker-compose exec web python manage.py collectstatic --no-input --clear
+docker-compose exec web python manage.py makemigrations api
+docker-compose exec web python manage.py migrate --noinput
+docker-compose exec web python manage.py createsuperuser
