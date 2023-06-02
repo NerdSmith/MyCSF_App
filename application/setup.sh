d5d2b15109927e3a71a0e724f814057dbb3db48b
Nikita Merzlyakov <merzlyakov02@gmail.com>
2023-06-02 05:27:34 +0300

    modified setup

--- application/setup.sh
+++ application/setup.sh
@@ -1,6 +1,7 @@
-docker-compose down -v --remove-orphans --rmi all
+# docker-compose down -v --remove-orphans --rmi all
+# docker-compose down --rmi all
 docker-compose up -d --build
 docker-compose exec web python manage.py collectstatic --no-input --clear
 docker-compose exec web python manage.py makemigrations api
 docker-compose exec web python manage.py migrate --noinput
-docker-compose exec web python manage.py createsuperuser
+docker-compose exec web python manage.py createsuperuser --username=ns --email=merzlyakov02@gmail.com --noinput
